a265e1d85e3efe2fffc1062f226ffbbf9a9b638c
larissavolfart <larissavolfart@gmail.com>
2025-05-23 17:34:35 -0300

    Atualiza server.js para usar porta dinâmica no deploy

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // 1. Serve os arquivos estáticos da pasta frontend
 app.use(express.static(path.join(__dirname, '../frontend')));
